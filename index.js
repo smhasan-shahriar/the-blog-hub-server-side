7c13fd53f81432d6231d8eb5d708a54210ba6342
Ador Shahriar <ador.shahriar@gmail.com>
2023-11-05 10:30:07 +0600

    basic express app created

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const port = process.env.PORT || 5000;
+
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('server connected')
+})
+
+app.listen(port, ()=>{
+    console.log(`server connected on ${port}`)
+})
