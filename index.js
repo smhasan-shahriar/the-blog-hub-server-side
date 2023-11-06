8eb4d98aaa476e80c352347e7e5dd4d03cebd974
Ador Shahriar <ador.shahriar@gmail.com>
2023-11-07 01:04:14 +0600

    recent blog retreive from database implemented

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-const express = require('express');
+const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
-
 
 const app = express();
 
-
-// middleware 
-app.use(cors())
-app.use(express.json())
-
-
-
+// middleware
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.rsv6fks.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -23,7 +18,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 // async function run() {
@@ -43,74 +38,81 @@ const client = new MongoClient(uri, {
 const database = client.db("blogDB");
 const blogCollection = database.collection("allBlogs");
 
-// retrieves all blogs 
-app.get('/allblogs', async(req, res)=> {
-    try{
-        const cursor= blogCollection.find();
-        const allBlogs = await cursor.toArray();
-        res.send(allBlogs)
-    }
-    catch(error){
-        console.log(error)
-      }
-})
+// retrieves all blogs
+app.get("/allblogs", async (req, res) => {
+  try {
+    const cursor = blogCollection.find();
+    const allBlogs = await cursor.toArray();
+    res.send(allBlogs);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-// retrieve single blog details 
-app.get('/blogs/:id', async(req, res)=> {
-    try{
-        const id = req.params.id; 
-        const query = {_id: new ObjectId(id)}
-        const blog = await blogCollection.findOne(query);
-        res.send(blog);
-    }
-    catch(error){
-        console.log(error)
-      }
-})
+// retrieve single blog details
+app.get("/blogs/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const blog = await blogCollection.findOne(query);
+    res.send(blog);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-// adding new blog by users 
-app.post('/addblog', async (req, res)=> {
-    try{
-      const newBlog = req.body;
-      const result = await blogCollection.insertOne(newBlog)
-      res.send(result)  
-      
-    }
-    catch(error){
-      console.log(error)
-    }
-   
-  })
+// retrieve recent 6 blogs
+app.get("/recentblogs", async (req, res) => {
+  try {
+    const query = {};
+    const options = {
+        sort: {time: -1}
+    };
+    const cursor = blogCollection.find(query,options);
+    const result = (await cursor.toArray()).slice(0, 6);
+    res.send(result)
+  } catch (error) {
+    console.log(error);
+  }
+});
 
+// adding new blog by users
+app.post("/addblog", async (req, res) => {
+  try {
+    const newBlog = req.body;
+    const result = await blogCollection.insertOne(newBlog);
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 // update blog by user
-app.put('/updateblog/:id', async(req, res) => {
-    try{
-        const id = req.params.id; 
-        const filter = {_id: new ObjectId(id)};
-        const updatedBlog = req.body;
-        const options = {upsert: false};
-        const updateBlog = {
-            $set: {
-                title: updatedBlog.title,
-                category: updatedBlog.category,
-                short: updatedBlog.short, 
-                long: updatedBlog.long
-            }
+app.put("/updateblog/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const filter = { _id: new ObjectId(id) };
+    const updatedBlog = req.body;
+    const options = { upsert: false };
+    const updateBlog = {
+      $set: {
+        title: updatedBlog.title,
+        category: updatedBlog.category,
+        short: updatedBlog.short,
+        long: updatedBlog.long,
+      },
+    };
+    const result = await blogCollection.updateOne(filter, updateBlog, options);
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-        }
-        const result = await blogCollection.updateOne(filter, updateBlog, options);
-        res.send(result)
-    }
-    catch(error){
-        console.log(error)
-      }
-})
+app.get("/", (req, res) => {
+  res.send("server connected");
+});
 
-app.get('/', (req, res) => {
-    res.send('server connected')
-})
-
-app.listen(port, ()=>{
-    console.log(`server connected on ${port}`)
-})
+app.listen(port, () => {
+  console.log(`server connected on ${port}`);
+});
