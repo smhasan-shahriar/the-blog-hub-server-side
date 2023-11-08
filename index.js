d490b53305682a70b8e752b09711f7714293a6f7
Ador Shahriar <ador.shahriar@gmail.com>
2023-11-08 13:52:50 +0600

    added local links for cors origin

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const app = express();
 app.use(cors({
   origin: [
       "http://localhost:5173",
+      "http://localhost:5174",
+      "http://localhost:5175",
       "https://sensational-taffy-bb51aa.netlify.app"
       
   ],
