482bade31f62a9c445d0444e3c8acdd46306c45f
Ador Shahriar <ador.shahriar@gmail.com>
2023-11-08 20:32:50 +0600

    formatting issue optimized

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
-const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser');
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const port = process.env.PORT || 5000;
@@ -10,34 +10,33 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 // middleware
-app.use(cors({
-  origin: [
+app.use(
+  cors({
+    origin: [
       "http://localhost:5173",
-      "https://sensational-taffy-bb51aa.netlify.app"
-      
-  ],
-  credentials: true
-}));
+      "https://sensational-taffy-bb51aa.netlify.app",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
-// verify token middleware creation 
-const verifyToken = (req, res, next) =>{
+// verify token middleware creation
+const verifyToken = (req, res, next) => {
   const token = req?.cookies?.token;
 
-  if(!token){
-      return res.status(401).send({message: 'unauthorized access'})
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
   }
-  jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) =>{
-      if(err){
-          return res.status(401).send({message: 'unauthorized access'})
-      }
-      req.user = decoded;
-      next();
-  })
- 
-}
-
+  jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.rsv6fks.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -51,44 +50,46 @@ const client = new MongoClient(uri, {
 });
 const dbConnect = async () => {
   try {
-      client.connect()
-      console.log('DB Connected Successfully✅')
+    client.connect();
+    console.log("DB Connected Successfully✅");
   } catch (error) {
-      console.log(error.name, error.message)
+    console.log(error.name, error.message);
   }
-}
-dbConnect()
+};
+dbConnect();
 //database collection creation
 const database = client.db("blogDB");
 const blogCollection = database.collection("allBlogs");
 const wishlistCollection = database.collection("wishlist");
 const userCollection = database.collection("users");
-const commentCollection = database.collection("comments")
+const commentCollection = database.collection("comments");
 app.get("/", (req, res) => {
   res.send("server connected");
 });
 
-// json web token creation 
+// json web token creation
 
-app.post('/jwt', async (req, res) => {
+app.post("/jwt", async (req, res) => {
   const user = req.body;
-  const token = jwt.sign(user, process.env.TOKEN_SECRET, { expiresIn: '1h' });
+  const token = jwt.sign(user, process.env.TOKEN_SECRET, { expiresIn: "1h" });
   res
-  .cookie('token', token, {
+    .cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-  })
-  .send({
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    })
+    .send({
       status: true,
-  })
-})
+    });
+});
 
 // clearing cookies when user logs out
-app.post('/logout', async (req, res) => {
+app.post("/logout", async (req, res) => {
   const user = req.body;
-  res.clearCookie('token', { maxAge: 0, sameSite: "none", secure: true }).send({ success: true })
-})
+  res
+    .clearCookie("token", { maxAge: 0, sameSite: "none", secure: true })
+    .send({ success: true });
+});
 
 // retrieves all blogs
 app.get("/allblogs", async (req, res) => {
@@ -102,7 +103,7 @@ app.get("/allblogs", async (req, res) => {
 });
 
 // retrieve single blog details
-app.get("/blogs/:id", verifyToken,  async (req, res) => {
+app.get("/blogs/:id", verifyToken, async (req, res) => {
   try {
     const id = req.params.id;
     const query = { _id: new ObjectId(id) };
@@ -156,24 +157,20 @@ app.get("/featured", async (req, res) => {
 app.get("/wishlist", verifyToken, async (req, res) => {
   try {
     const email = req.query.email;
-    if(req.user.email !== req.query.email){
-      return res.status(403).send({message: 'forbidden access'})
-  }
-    if(email){
-      const query = {userEmail: email};
+    if (req.user.email !== req.query.email) {
+      return res.status(403).send({ message: "forbidden access" });
+    }
+    if (email) {
+      const query = { userEmail: email };
+      const cursor = wishlistCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    } else {
+      const query = {};
       const cursor = wishlistCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
     }
-    else{
-      const query = {}
-      const cursor = wishlistCollection.find(query);
-      const result = await cursor.toArray();
-      res.send(result);
-    }
-
-  
-    
   } catch (error) {
     console.log(error);
   }
@@ -255,22 +252,19 @@ app.post("/comments", async (req, res) => {
   } catch (error) {
     console.log(error);
   }
-})
+});
 //getting comments from the database
 app.get("/comments/:id", async (req, res) => {
   try {
     const id = req.params.id;
-    const query = { blogId: id};
+    const query = { blogId: id };
     const cursor = commentCollection.find(query);
     const result = await cursor.toArray();
     res.send(result);
   } catch (error) {
     console.log(error);
   }
-})
-
-
-
+});
 
 app.listen(port, () => {
   console.log(`server connected on ${port}`);
